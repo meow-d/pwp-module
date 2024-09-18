60da476b28cbbb9f9f5c0f4428d830284674f950
meow-d <isaacwenyi@gmail.com>
2024-09-18 09:59:23 +0800

    a

--- tutorial/1c/2.py
+++ tutorial/1c/2.py
@@ -1,17 +1,17 @@
-restaurants = [
-    ["Corner Café", True, True, True],
-    ["Main Street Pizza Company", True, False, True],
-    ["The Chef’s Kitchen", True, True, True],
-    ["Joe’s Gourmet Burgers", False, False, False],
-    ["Mama’s Fine Italian", True, False, False],
-]
-restaurant_criteria = [False]
+restaurants = {
+    "Corner Café": [True, True, True],
+    "Main Street Pizza Company": [True, False, True],
+    "The Chef’s Kitchen": [True, True, True],
+    "Joe’s Gourmet Burgers": [False, False, False],
+    "Mama’s Fine Italian": [True, False, False],
+}
+restaurant_criteria = []
 
 
 def user_input(personal_requirement):
     print(f"Is anyone {personal_requirement}? [Y/n]: ", end="")
     response = None
-    while not (response == (True or False)):
+    while response is None:
         response = input()
         if response == None:
             response = True
@@ -27,23 +27,18 @@ def user_input(personal_requirement):
 
 user_input("a vegan")
 user_input("a vegetarian")
-user_input("on a gluten-free diet")
+user_input("a gluten-free guy")
 
 print(restaurant_criteria)
+restaurant_choices = restaurants.copy()
 
-restaurant_choices = restaurants
-items_to_be_removed_becuase_python_is_a_bitch = []
-for restaurant in restaurants:
-    for i in range(1, 4):
-        if restaurant_criteria[i]:
-            if not restaurant[i]:
-                # for some reason modifing the list here messes up the loop, so i'm modifying outside of the loop
-                items_to_be_removed_becuase_python_is_a_bitch.append(restaurant)
-
-for item in items_to_be_removed_becuase_python_is_a_bitch:
-    if item in restaurant_choices:
-        restaurant_choices.remove(item)
+for restaurant, criteria in restaurants.items():
+    for i, meets_criteria in enumerate(criteria):
+        if restaurant_criteria[i] and not meets_criteria:
+            if restaurant in restaurant_choices:
+                del restaurant_choices[restaurant]
+                break
 
 print("\nRestaurant choices:")
 for restaurant in restaurant_choices:
-    print(restaurant[0])
+    print(restaurant)
